f46224515813f8c489371a41da512f53aff51813
Jonathan Rowell <rowell.jonathan@yahoo.com>
2019-03-26 20:27:32 -0500

    Adding components

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end', paddingRight: '1em'}}>
+            <p className='f3 underline black dim link pointer'>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
