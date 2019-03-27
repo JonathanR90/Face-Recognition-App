b8d933400f112f1864a9649b4d49bee6cac9ba25
Jonathan Rowell <rowell.jonathan@yahoo.com>
2019-03-26 21:11:15 -0500

    added Particles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
+const particleParams = {
+  particles: {
+    'number': {
+      'value': 30,
+      density: {
+        enable: true,
+        value_area: 80
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles' 
+          params={particleParams}
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
